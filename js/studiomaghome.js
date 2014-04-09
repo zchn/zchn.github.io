dfa0765dac852393b7d221bb8a28189be2187d3f
Kevin Z. Chen <kevinzc@berkeley.edu>
2014-04-08 23:24:10 -0700

    isediting

--- js/studiomaghome.js
+++ js/studiomaghome.js
@@ -1,4 +1,12 @@
 videoiframe = "<iframe src=\"\/\/player.vimeo.com\/video\/91481101?byline=0&amp;portrait=0&amp;autoplay=1\" width=\"500\" height=\"281\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen><\/iframe>"
 function mydivonload(){
+    if isEditing(){
+	console.log("in editing mode");
+    }else{
+	console.log("in production mode");
+    }
 }
 
+function isEditing(){
+    return document.getElementById("w-editor-top");
+}
