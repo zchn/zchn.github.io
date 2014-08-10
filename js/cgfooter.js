f6dce79bdbb4a0200a43cc13424fb3bece90604e
Kevin Z Chen <kevinchn@cs.berkeley.edu>
2014-08-10 02:02:36 -0400

    Update cgfooter.js

--- js/cgfooter.js
+++ js/cgfooter.js
@@ -1,2 +1,5 @@
 console.log('footerjs');
-document.body.style.background="#f3f3f3 url('http://cdn1.editmysite.com/uploads/2/6/7/2/26720964/background-images/1940869920.jpg') no-repeat right top";
+mainwrap = document.getElementById('main-wrap');
+if(mainwrap !== null){
+  mainwrap.style.background = "#f3f3f3 url('http://cdn1.editmysite.com/uploads/2/6/7/2/26720964/background-images/1940869920.jpg') right top";
+}
