3f6bb108b39aa0de74540d332cfda83aa8e3eb88
Kevin Z Chen <kevinchn@cs.berkeley.edu>
2014-08-10 02:13:10 -0400

    Update cgfooter.js

--- js/cgfooter.js
+++ js/cgfooter.js
@@ -1,5 +1,5 @@
 console.log('footerjs');
 mainwrap = document.getElementById('main-wrap');
-if(window.location.pathname != '/' && mainwrap !== null){
+if(window.location.pathname === '/video-projects.html' && mainwrap !== null){
   mainwrap.style.background = "#000000 url('http://cdn1.editmysite.com/uploads/2/6/7/2/26720964/background-images/1940869920.jpg') fixed no-repeat center top";
 }
