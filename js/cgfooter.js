b93a4d991ad3213b8823b4623f332e33a19175b4
Kevin Z Chen <kevinchn@cs.berkeley.edu>
2014-08-10 02:41:35 -0400

    Update cgfooter.js

--- js/cgfooter.js
+++ js/cgfooter.js
@@ -1,7 +1,7 @@
 console.log('footerjs');
 mainwrap = document.getElementById('main-wrap');
 cgbgurl = 'http://zchn.github.io/cg/bgchangstreet.jpg';
-if(window.location.pathname === '/video-projects.html' && mainwrap !== null){
+if(window.location.pathname === '/resume.html' && mainwrap !== null){
   mainwrap.style.background = "#000000 url('"+cgbgurl+"') fixed no-repeat center top";
   mainwrap.style['-webkit-background-size'] = 'cover';
   mainwrap.style['-moz-background-size'] = 'cover';
