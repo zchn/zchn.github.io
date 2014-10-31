937734489f0300da4a43d8592af5e4cac6958672
Kevin Z Chen <kevin.zchn@gmail.com>
2014-10-30 18:03:16 -0700

    Update gradescope.user.js

--- js/gradescope.user.js
+++ js/gradescope.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gradescope next page
 // @namespace    http://zchn.github.io/
-// @version      0.3
+// @version      0.4
 // @description  enter something useful
 // @author       Kevin Chen
 // @match        https://www.gradescope.com/courses/*/questions/*/submissions/*/grade
@@ -10,8 +10,8 @@
 // @updateURL    http://zchn.github.io/js/gradescope.user.js
 // ==/UserScript==
 
-$(document).keydown(function(e){
-  if (e.which === 221){
+$(document).keypress(function(e){
+  if (e.which == 221){
     $('.page-icon.disabled.active').next().children()[0].click();
     }
   }
