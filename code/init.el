9bb3f607f0b5fe76b574db3d1e6853353f857931
Kevin Chen <kevin.zchn@gmail.com>
2015-08-09 13:06:17 -0400

    add scripts

---
+++ code/init.el
@@ -0,0 +1,3 @@
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
